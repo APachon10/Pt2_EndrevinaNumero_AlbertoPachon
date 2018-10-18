82c7dd58fd85dcab1ded13cd4a725d6e2ae49a13
APachon10 <Pachondestroy95@gmail.com>
2018-10-18 20:21:11 +0200

    Nombre: Alberto Pachon Practica 2 --> Endrevina Numero

--- app/src/main/java/com/example/tnb_20/endrevinanumero/MainActivity.java
+++ app/src/main/java/com/example/tnb_20/endrevinanumero/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.tnb_20.endrevinanumero;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -29,7 +31,11 @@ public class MainActivity extends AppCompatActivity {
             /*Programamos */
             @Override
             public void onClick(View v) {
-                if(valorAleatorio == R.id.Num_Usuario){
+
+                EditText et = findViewById(R.id.Num_Usuario);
+                String s = et.getText().toString();
+                int valor = Integer.parseInt(s);
+                if(valorAleatorio == valor){
                     Context context = getApplicationContext();
                     CharSequence text = "You Win ";
                     int duration = Toast.LENGTH_SHORT;
@@ -47,5 +53,12 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, TablaRecords.class);
+        EditText editText = findViewById(R.id.Num_Usuario);
+        String message = editText.getText().toString();
+        intent.putExtra("MSG1", message);
+        startActivity(intent);
     }
 }
