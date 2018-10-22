ac5ccf44bad9ae79c519ca8ddd9e436aea776500
APachon10 <Pachondestroy95@gmail.com>
2018-10-22 20:26:40 +0200

    Nombre: Alberto Pachon Practica 2 --> Endrevina Numero

--- app/src/main/java/com/example/tnb_20/endrevinanumero/MainActivity.java
+++ app/src/main/java/com/example/tnb_20/endrevinanumero/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tnb_20.endrevinanumero;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +16,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     protected int valorAleatorio;
     protected int intentos;
+    protected String name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,16 +45,48 @@ public class MainActivity extends AppCompatActivity {
                     Toast toast = Toast.makeText(context, text, duration);
                     toast.show();
                 }else{
-                    Context context = getApplicationContext();
-                    CharSequence text = "You Lose ";
-                    int duration = Toast.LENGTH_SHORT;
+                    if(valorAleatorio > valor){
+                        Context context = getApplicationContext();
+                        CharSequence text = "El valor es Incorrecto,El Numero que buscas es mas Grande "+Integer.toString(valorAleatorio);
+                        int duration = Toast.LENGTH_SHORT;
 
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }else {
+                        Context context = getApplicationContext();
+                        CharSequence text = "El valor es Incorrecto,El Numero que buscas es mas Pequeño";
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
                     intentos++;
                 }
             }
         });
+
+        final Button record = findViewById(R.id.Records);
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.dialog);
+                dialog.setTitle("Title");
+
+                Button button = dialog.findViewById(R.id.button_ok);
+                button.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+
+                        EditText edit=(EditText)dialog.findViewById(R.id.user_name);
+                        String text=edit.getText().toString();
+
+                        dialog.dismiss();
+                        name=text;
+                    }
+                });
+                dialog.show();
+            }
+        }
     }
     public void sendMessage(View view) {
         Intent intent = new Intent(this, TablaRecords.class);
