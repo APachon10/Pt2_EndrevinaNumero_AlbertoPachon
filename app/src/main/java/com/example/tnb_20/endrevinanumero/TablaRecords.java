169ac9b0f8fe60b1869e07e86f594de72f7db25f
APachon10 <Pachondestroy95@gmail.com>
2018-11-12 19:55:11 +0100

    Nombre: Alberto Pachon Practica 2 --> Endrevina Numero

--- app/src/main/java/com/example/tnb_20/endrevinanumero/TablaRecords.java
+++ app/src/main/java/com/example/tnb_20/endrevinanumero/TablaRecords.java
@@ -2,12 +2,16 @@ package com.example.tnb_20.endrevinanumero;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class TablaRecords extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabla_records);
+
+
+
     }
 }
