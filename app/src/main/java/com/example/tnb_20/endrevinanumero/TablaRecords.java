f00f2e8ab3036454cde9006b3941c207307d203a
APachon10 <Pachondestroy95@gmail.com>
2018-12-13 20:21:40 +0100

    Tabla Records Se llena

--- app/src/main/java/com/example/tnb_20/endrevinanumero/TablaRecords.java
+++ app/src/main/java/com/example/tnb_20/endrevinanumero/TablaRecords.java
@@ -2,13 +2,37 @@ package com.example.tnb_20.endrevinanumero;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
+
+import java.util.ArrayList;
 
 public class TablaRecords extends AppCompatActivity {
+
+    public int intentos;
+    public String name;
+
+    class Record {
+        public Record(int _intents, String _nom) {
+            intentos = _intents;
+            name = _nom;
+        }
+    }
+    ArrayList<Record> records;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabla_records);
+
+        // Inicialitzem model
+        records = new ArrayList<Record>();
+        
+        // busquem la ListView i li endollem el ArrayAdapter
+        TextView lv = (TextView) findViewById(R.id.tabla);
+        lv.setText(name + " " +intentos);
+        }
+
+
     }
-}
